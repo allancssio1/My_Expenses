0741dd028b94fa7072333f4e150a92276164f0f4
allancassio <allan.cassio1@gmail.com>
2023-07-28 17:08:55 -0300

    restructured files orders

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './module/user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { ExpensesModule } from './module/expenses/expenses.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AppModule, UserModule, PrismaModule, ExpensesModule],
+  imports: [AppModule, PrismaModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
