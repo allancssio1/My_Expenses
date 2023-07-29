6256372bcc247f390bb83033b0091948cfe7b7b9
allancassio <allan.cassio1@gmail.com>
2023-07-29 16:48:34 -0300

    create module controller service auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { ExpensesModule } from './expenses/expenses.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, ExpensesModule],
+  imports: [PrismaModule, UserModule, ExpensesModule, AuthModule],
   controllers: [],
   providers: [],
 })
