576d2b7a2a7bc2ec8b1ba203894e996c8bee973c
allancassio <allan.cassio1@gmail.com>
2023-07-30 20:38:14 -0300

    implement token strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,20 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { ExpensesModule } from './expenses/expenses.module';
 import { AuthModule } from './auth/auth.module';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [PrismaModule, UserModule, ExpensesModule, AuthModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
