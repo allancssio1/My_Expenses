32706b43a5fcd1dbeeb2521736441968c4ce23ec
allancassio <allan.cassio1@gmail.com>
2023-07-31 12:38:01 -0300

    remove dto unused

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,13 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { ExpensesModule } from './expenses/expenses.module';
 import { AuthModule } from './auth/auth.module';
-import { AppService } from './app.service';
-import { AppController } from './app.controller';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [PrismaModule, UserModule, ExpensesModule, AuthModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [
-    AppService,
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
